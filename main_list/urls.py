cca46709114b65ac64d4720c1687012353d1658d
NataB-hub <natalia.bcwz@gmail.com>
2020-07-06 14:54:39 +0300

    Add view, url, templates for app main_list.

---
+++ main_list/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from main_list import views
+
+urlpatterns = [
+    path('', views.main_list, name='main_list'),
+    ]
--- main_list/views.py
+++ main_list/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from pip._vendor import requests
+from main_list.models import Blog
+from django.utils import timezone
+from datetime import date
 
-# Create your views here.
+def main_list(request):
+    blogs = Blog.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    variable = str(date.today().month) + '/' + str(date.today().day)
+    api_url = 'http://numbersapi.com/' + variable
+    params = {
+        'type': 'date',
+        'json': True
+    }
+    res = requests.get(api_url, params=params).json()
+    return render(request, 'main_list.html', {'blogs': blogs, 'res': res, 'date': date.today()})
